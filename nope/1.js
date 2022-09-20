726a1f0bef642e15adbe4cc0ee29e6630af80713
zayn <mdrahul6967@gmail.com>
2022-09-21 04:34:18 +0600

    hjh

--- nope/1.js
+++ nope/1.js
@@ -45,6 +45,8 @@ function imgdiv(bname, bimg, q7pp,size,t1,t2) {
         chidiv.style.width = g * .6 + 'px'
 
         image.style.width = g * .6 + 'px'
+        image.style.height = h * .7 + 'px'
+
 
         iiidiv.style.height = h * .7 + 'px'
         iiidiv.style.overflow = 'hidden'
